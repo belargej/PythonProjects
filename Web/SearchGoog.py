bbaba925f636c2cb5c3235d8ecd7f9c3c99a07b2
Joe Belarge <belarge@nscl.msu.edu>
2015-11-05 19:58:43 -0500

    python accessing the web

---
+++ Web/SearchGoog.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://www.google.com")
+assert "Google" in driver.title
+search_bar = driver.find_element_by_name("q")
+search_bar.send_keys("cnnmoney"+Keys.RETURN)
