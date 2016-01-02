63c93cef5afdc5fd9fc2e679f861a3cd9cd943a1
Joe Belarge <belarge@nscl.msu.edu>
2016-01-02 16:44:58 -0500

    working more on line2

--- RandWalk/Line2.py
+++ RandWalk/Line2.py
@@ -6,6 +6,7 @@ import random
 
 ADDVAL = 15
 XSTEP = 5
+STEPS = 70
 
 class Ex(Frame):
     def __init__(self,parent):
@@ -15,8 +16,9 @@ class Ex(Frame):
         self.x_current = 20
         self.y_before = 200
         self.y_current = 200
+        self.FirstRun = True
         self.initUI()
-        self.runRandWalk(70)
+        self.runRandWalk(STEPS)
 
     def initUI(self):
         self.parent.title("Random Walk Lines 1")
@@ -35,19 +37,33 @@ class Ex(Frame):
                                    command=self.newWalk)
         self.closeButton   = Button(self,text="Close",
                                     command=self.closeAll)
+
+        # pack the buttons here
         self.closeButton.pack(side=RIGHT,padx=5,pady=5)
         self.newWalkButton.pack(side=RIGHT)
 
 
     def newWalk(self):
         print("Walk anew")
+        for x in range(0,STEPS):
+            getTag = "step"+str(x)
+            self.canvas.coords(getTag,(0,0,0,0))
+
+
+        self.x_current = 20
+        self.x_before  = 20
+        self.y_current = 200
+        self.x_before  = 200
+        self.runRandWalk(STEPS)
+        print ("did this run?")
 
     def closeAll(self):
         Frame.quit(self)
 
-    def RandWalk(self):
+    def RandWalk(self,whichStep):
         r = random.randint(0,1)
         addval = 0;
+        toTag = "step"+str(whichStep)
         if r == 0:
             addval = ADDVAL
         else:
@@ -60,20 +76,24 @@ class Ex(Frame):
             if self.y_before == 200:
                 self.canvas.create_line(self.x_before,self.y_before,
                                         self.x_current,self.y_current,
-                                        fill = "green", width = 5)
+                                        fill = "green", width = 5,
+                                        tags=toTag)
             else:
                 self.canvas.create_line(self.x_before,self.y_before,
                                         self.x_current,self.y_current,
-                                        fill = "green", width = 5)
+                                        fill = "green", width = 5,
+                                        tags=toTag)
         else:
             if self.y_before<200:
                 self.canvas.create_line(self.x_before,self.y_before,
                                         self.x_current,self.y_current,
-                                        fill = "green", width = 5)
+                                        fill = "green", width = 5,
+                                        tags=toTag)
             else:
                 self.canvas.create_line(self.x_before,self.y_before,
                                         self.x_current,self.y_current,
-                                        fill = "red", width = 5)
+                                        fill = "red", width = 5,
+                                        tags=toTag)
 
         self.canvas.pack(fill=BOTH,expand=1)
         self.x_before = self.x_current
@@ -81,7 +101,7 @@ class Ex(Frame):
 
     def runRandWalk(self,steps):
         for x in range(0,steps):
-            self.RandWalk()
+            self.RandWalk(x)
         
 
 def main():
